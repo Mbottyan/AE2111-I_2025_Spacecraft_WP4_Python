c7d7c5e8b5755f3e93ca7d3de60e23d2466e1f03
Antoniimilewski-ctrl <antoni.i.milewski@gmail.com>
2025-11-18 15:44:20 +0100

    Created fastener class, fastener list. Generated 4 random fasteners with locations with 1cm diameter. Made function to calculate cg for the fasteners

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+
+import math
+import random
+
+
+
+
+
+
+
 #Constant forces from FBD
 Fx = 10 #N
 Fy = 20 #N
@@ -5,3 +15,41 @@ Fz = 30 #N
 Mx = 5  #Nm
 My = 15 #Nm
 Mz = 25 #Nm
+
+
+
+
+class Fastener:
+    def __init__(self, Diameter, x_coord, z_coord):
+        #sets coordinates and dimensions for each fastener in our design
+        self.Diameter=float(Diameter)
+        self.x_coord=float(x_coord)
+        self.z_coord=float(z_coord)
+    # give the coordinates weighted and areas of fastener of cg calculation
+    def provide_x_weighted_average(self):
+        self.A=(math.pi)*(self.Diameter*0.5)**2
+        return (self.A*self.x_coord), (self.A)
+    def provide_z_weighted_average(self):
+        self.A=(math.pi)*(self.Diameter*0.5)**2
+        return (self.A*self.z_coord), (self.A)
+        
+
+    
+Fasteners=[]
+#create list for all fastener instances
+#generate random fasteners, TO BE REPLACED WITH REAL ONES!
+for i in range(4):
+    Fasteners.append(Fastener(0.01,random.randint(0,5),random.randint(0,5)))
+
+def cg_location():
+    x_num_sum=0
+    x_den_sum=0
+    z_num_sum=0
+    z_den_sum=0
+    for item in Fasteners:
+        x_num_sum+=(item.provide_x_weighted_average()[0])
+        x_den_sum+=(item.provide_x_weighted_average()[1])
+        z_num_sum+=(item.provide_z_weighted_average()[0])
+        z_den_sum+=(item.provide_z_weighted_average()[1])
+    return (x_num_sum/x_den_sum, z_num_sum/z_den_sum)
+print(cg_location())
